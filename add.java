fe55d64bf15491ea8e0031462a75bd5baaf584fd
Uma Bharathi <umabharathi2007.ok@gmail.com>
2026-08-28 21:06:13 +0530

    Added EvenorOdd program

--- add.java
+++ add.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class add{
+public class Add{
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first number:");
